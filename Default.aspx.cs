9249db6da40ffed42476eaf976a737021eb27f77
Colin Mahoney <cdmahoney+githubrepo@gmail.com>
2012-09-06 13:28:00 +0200

    Initial commit

---
+++ Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+//using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EchoRequest
+{
+	public partial class _Default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			Response.Write("<h2>Query params</h2>");
+			foreach (string paramKey in Request.QueryString.AllKeys)
+			{
+				Response.Write(paramKey + " = " + Request.QueryString[paramKey] + "<br>");
+			}
+
+			Response.Write("<h2>Files</h2>");
+			HttpFileCollection files = Request.Files;
+			foreach (string key in files.AllKeys)
+			{
+				Response.Write("File: " + Server.HtmlEncode(key) + "<br>");
+				HttpPostedFile file = files[key];
+				byte[] bytes = new byte[file.ContentLength];
+				file.InputStream.Read(bytes, 0, bytes.Length);
+				string base64 = Convert.ToBase64String(bytes);
+				Response.Write("Content: " + base64 + "<br>");
+			}
+		}
+	}
+}
