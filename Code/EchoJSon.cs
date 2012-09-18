17a2cc91c7752a03d9a6b589d56a6b9a184f5a85
Colin Mahoney <cdmahoney+githubrepo@gmail.com>
2012-09-18 12:30:00 +0200

    JS Encode content

--- Code/EchoJSon.cs
+++ Code/EchoJSon.cs
@@ -133,7 +133,9 @@ namespace EchoRequest.Code
 		public static void WriteKeyValue(HttpResponse response, ref string comma, string key, object value)
 		{
 			// TODO: Escape value?
-			string json = string.Format("{0}\"{1}\": \"{2}\"", comma, key, value);
+			var encoded = EncodeJsString(value != null ? value.ToString() : string.Empty);
+			string json = string.Format("{0}\"{1}\": \"{2}\"", comma, key, encoded);
+			//string json = string.Format("{0}\"{1}\": \"{2}\"", comma, key, value);
 			response.Write(json);
 			comma = ", ";
 		}
@@ -266,6 +268,64 @@ namespace EchoRequest.Code
 				}
 			}
 			sb.Append("]}");
+			return sb.ToString();
+		}
+		/// <summary>
+		/// Encodes a string to be represented as a string literal. The format
+		/// is essentially a JSON string.
+		/// 
+		/// The string returned includes outer quotes 
+		/// Example Output: "Hello \"Rick\"!\r\nRock on"
+		/// 
+		/// See:
+		///		http://www.west-wind.com/weblog/posts/2007/Jul/14/Embedding-JavaScript-Strings-from-an-ASPNET-Page
+		/// </summary>
+		/// <param name="s"></param>
+		/// <returns></returns>
+		private static string EncodeJsString(string s)
+		{
+			StringBuilder sb = new StringBuilder();
+			//sb.Append("\"");
+			foreach (char c in s)
+			{
+				switch (c)
+				{
+					case '\"':
+						sb.Append("\\\"");
+						break;
+					case '\\':
+						sb.Append("\\\\");
+						break;
+					case '\b':
+						sb.Append("\\b");
+						break;
+					case '\f':
+						sb.Append("\\f");
+						break;
+					case '\n':
+						sb.Append("\\n");
+						break;
+					case '\r':
+						sb.Append("\\r");
+						break;
+					case '\t':
+						sb.Append("\\t");
+						break;
+					default:
+						int i = (int)c;
+						if (i < 32 || i > 127)
+						{
+							sb.AppendFormat("\\u{0:X04}", i);
+						}
+						else
+						{
+							sb.Append(c);
+						}
+						break;
+				}
+			}
+			//sb.Append("\"");
+
 			return sb.ToString();
 		}
 	}
