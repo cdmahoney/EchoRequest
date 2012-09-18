644ad471a4d3b5a8f2f2e2355c00841134a833c4
Colin Mahoney <cdmahoney+githubrepo@gmail.com>
2012-09-18 17:06:06 +0200

    Added AppConfig

--- Code/EchoJSon.cs
+++ Code/EchoJSon.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Web;
 using System.Web.UI;
 
+using EchoRequest.Code.Support;
+
 namespace EchoRequest.Code
 {
 	public class EchoJSon: Page
@@ -17,7 +19,9 @@ namespace EchoRequest.Code
 		{
 			None,
 			Create,
-			GetTypes
+			GetTypes,
+			GetAppConfigServers,
+			GetAppConfig
 		}
 
 
@@ -47,9 +51,15 @@ namespace EchoRequest.Code
 				case Service.GetTypes:
 					ProcessViamapTypesRequest(Response, Request);
 					break;
+				case Service.GetAppConfigServers:
+					Services.AppConfig.GetAppConfigServers(Response, Request);
+					break;
+				case Service.GetAppConfig:
+					Services.AppConfig.GetAppConfig(Response, Request);
+					break;
 				default:
 					throw new InvalidOperationException(string.Format("Requested service '{0}' not supported", serviceName));
-					break;
+					//break;
 			}
 
 			if(suffix.Length > 0)
@@ -133,7 +143,7 @@ namespace EchoRequest.Code
 		public static void WriteKeyValue(HttpResponse response, ref string comma, string key, object value)
 		{
 			// TODO: Escape value?
-			var encoded = EncodeJsString(value != null ? value.ToString() : string.Empty);
+			var encoded = SJavaScript.EncodeJsString(value != null ? value.ToString() : string.Empty);
 			string json = string.Format("{0}\"{1}\": \"{2}\"", comma, key, encoded);
 			//string json = string.Format("{0}\"{1}\": \"{2}\"", comma, key, value);
 			response.Write(json);
@@ -268,68 +278,6 @@ namespace EchoRequest.Code
 				}
 			}
 			sb.Append("]}");
-			return sb.ToString();
-		}
-		/// <summary>
-		/// Encodes a string to be represented as a string literal. The format
-		/// is essentially a JSON string.
-		/// 
-		/// The string returned includes outer quotes 
-		/// Example Output: "Hello \"Rick\"!\r\nRock on"
-		/// 
-		/// See:
-		///		http://www.west-wind.com/weblog/posts/2007/Jul/14/Embedding-JavaScript-Strings-from-an-ASPNET-Page
-		/// </summary>
-		/// <param name="s"></param>
-		/// <returns></returns>
-		private static string EncodeJsString(string s)
-		{
-			StringBuilder sb = new StringBuilder();
-			//sb.Append("\"");
-			foreach (char c in s)
-			{
-				switch (c)
-				{
-					case '\"':
-						sb.Append("\\\"");
-						break;
-					//case '\'':
-					//    // Added CDM 19/09/2012
-					//    sb.Append("\\'");
-					//    break;
-					case '\\':
-						sb.Append("\\\\");
-						break;
-					case '\b':
-						sb.Append("\\b");
-						break;
-					case '\f':
-						sb.Append("\\f");
-						break;
-					case '\n':
-						sb.Append("\\n");
-						break;
-					case '\r':
-						sb.Append("\\r");
-						break;
-					case '\t':
-						sb.Append("\\t");
-						break;
-					default:
-						int i = (int)c;
-						if (i < 32 || i > 127)
-						{
-							sb.AppendFormat("\\u{0:X04}", i);
-						}
-						else
-						{
-							sb.Append(c);
-						}
-						break;
-				}
-			}
-			//sb.Append("\"");
-
 			return sb.ToString();
 		}
 	}
---
+++ Code/Services/AppConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.IO;
+using System.Web;
+
+namespace EchoRequest.Code.Services
+{
+	public static class AppConfig
+	{
+		public static void GetAppConfigServers(HttpResponse response, HttpRequest request)
+		{
+		}
+		public static void GetAppConfig(HttpResponse response, HttpRequest request)
+		{
+			string file = "AppData\\AppConfigServers.json";
+			// Cheap and cheerful
+			byte[] content = File.ReadAllBytes(file);
+			response.OutputStream.Write(content, 0, content.Length);
+		}
+	}
+}
