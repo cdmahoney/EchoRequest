f4a2489826d9631e65ba673e1d8714086928003e
Colin Mahoney <cdmahoney+githubrepo@gmail.com>
2012-09-19 14:51:28 +0200

    Added GetConfig

--- Code/Services/AppConfig.cs
+++ Code/Services/AppConfig.cs
@@ -7,23 +7,40 @@ namespace EchoRequest.Code.Services
 {
 	public static class AppConfig
 	{
+		private const string ContentTypeJson = "application/json";
+
 		public static void GetAppConfigServers(HttpResponse response, HttpRequest request)
 		{
-			string path = "/App_Data/AppConfigServers.json";
-			string fullpath = HttpContext.Current.Server.MapPath(path);
-			string fullpath2 = request.MapPath(path);
+			Support.SLocalFile.StreamLocalFile("/App_Data/AppConfigServers.json", ContentTypeJson, response, request);
+			//string path = "/App_Data/AppConfigServers.json";
+			//string fullpath = HttpContext.Current.Server.MapPath(path);
+			//string fullpath2 = request.MapPath(path);
 
-			// Set content type
-			response.ContentType = "application/json";
-			//response.ContentType = "application/json; charset=utf-8";
+			//// Set content type
+			//response.ContentType = "application/json";
 
-			// Cheap and cheerful
-			// Write content as text to prevent having to worry about character encoding
-			string content = File.ReadAllText(fullpath);
-			response.Write(content);
+			//// Cheap and cheerful - not very efficient
+			//// Write content as text to prevent having to worry about character encoding
+			//string content = File.ReadAllText(fullpath);
+			//response.Write(content);
 		}
 		public static void GetAppConfig(HttpResponse response, HttpRequest request)
 		{
+			Support.SLocalFile.StreamLocalFile("/App_Data/AppConfig.json", ContentTypeJson, response, request);
 		}
+
+		//public static void GetLocalJson(string filepath, HttpResponse response, HttpRequest request)
+		//{
+		//    string fullpath = HttpContext.Current.Server.MapPath(filepath);
+		//    string fullpath2 = request.MapPath(path);
+
+		//    // Set content type
+		//    response.ContentType = "application/json";
+
+		//    // Cheap and cheerful - not very efficient
+		//    // Write content as text to prevent having to worry about character encoding
+		//    string content = File.ReadAllText(fullpath);
+		//    response.Write(content);
+		//}
 	}
 }
---
+++ Code/Support/SLocalFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+namespace EchoRequest.Code.Support
+{
+	public static class SLocalFile
+	{
+		public static void StreamLocalFile(string filepath, HttpResponse response, HttpRequest request)
+		{
+			StreamLocalFile(filepath, string.Empty, response, request);
+		}
+		public static void StreamLocalFile(string filepath, string contentType, HttpResponse response, HttpRequest request)
+		{
+			string fullpath = HttpContext.Current.Server.MapPath(filepath);
+			string fullpath2 = request.MapPath(filepath);
+
+			if (contentType.Length > 0)
+			{
+				// Set content type
+				response.ContentType = "application/json";
+			}
+
+			// Cheap and cheerful - not very efficient
+			// Write content as text to prevent having to worry about character encoding
+			string content = File.ReadAllText(fullpath);
+			response.Write(content);
+		}
+	}
+}
