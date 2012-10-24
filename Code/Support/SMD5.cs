756affce609c325180960c3a11daf004bcfd0fa5
Colin Mahoney <cdmahoney+githubrepo@gmail.com>
2012-10-24 13:01:29 +0200

    Added entry point

---
+++ Code/Support/SMD5.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EchoRequest.Code.Support
+{
+	public class SMD5
+	{
+		// ms-help://MS.MSDNQTR.v80.en/MS.MSDN.v80/MS.NETDEVFX.v20.en/cpref11/html/T_System_Security_Cryptography_MD5.htm
+		// Hash an input string and return the hash as
+		// a 32 character hexadecimal string.
+		public static string GetMd5Hash(string token)
+		{
+			// Create a new instance of the MD5CryptoServiceProvider object.
+			MD5 md5 = MD5.Create();
+
+			// Convert the input string to a byte array and compute the hash.
+			byte[] hash_valueAnt = md5.ComputeHash(Encoding.Default.GetBytes(token));
+
+			byte[] codigo = new byte[] { 57, 51, 50, 57, 52, 52 };
+
+			List<byte> tokenList = new List<byte>(hash_valueAnt.Length + codigo.Length);
+			tokenList.AddRange(hash_valueAnt);
+			tokenList.AddRange(codigo);
+			byte[] token2 = tokenList.ToArray();
+
+			byte[] hash_value = md5.ComputeHash(token2);
+			
+			char[] chash_value = new char[hash_value.Length];
+			for (int i = 0; i < hash_value.Length; i++)
+			{
+				if (hash_value[i] < 0)
+				{
+					chash_value[i] = (char)(hash_value[i] + 256);
+				}
+				else
+				{
+					chash_value[i] = (char)(hash_value[i]);
+				}
+			}
+
+			// Get Hex Hash
+			StringBuilder sb = new StringBuilder(chash_value.Length * 2);
+			for (int x = 0; x < chash_value.Length; x++)
+			{
+				String sAux = "00" + (0xff & chash_value[x]).ToString("x2"); //Integer.toHexString(0xff & chash_value[x]);
+				sb.Append(sAux.Substring(sAux.Length - 2,sAux.Length));
+			}
+
+			string result = sb.ToString();
+			return result;
+
+			//// Create a new Stringbuilder to collect the bytes
+			//// and create a string.
+			//StringBuilder sBuilder = new StringBuilder();
+
+			//// Loop through each byte of the hashed data 
+			//// and format each one as a hexadecimal string.
+			//for (int i = 0; i < data.Length; i++)
+			//{
+			//    sBuilder.Append(data[i].ToString("x2"));
+			//}
+
+			//// Return the hexadecimal string.
+			//return sBuilder.ToString();
+		}
+
+	}
+}
--- Services/Multas.asmx.cs
+++ Services/Multas.asmx.cs
@@ -8,33 +8,98 @@ namespace EchoRequest.Services
 	/// <summary>
 	/// Summary description for Multas
 	/// </summary>
-	[WebService(Namespace = "http://echorequest.apphb.com//")]
+	//[WebService(Namespace = "http://echorequest.apphb.com//")]
+	[WebService(Namespace = "http://impl.multas.gtwin.conecta")]
 	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
 	[System.ComponentModel.ToolboxItem(false)]
 	public class Multas : System.Web.Services.WebService
 	{
 
 		[WebMethod]
-		public string Login(string user, string password)
+		public string login(string user, string password)
 		{
-			string datetime = DateTime.Now.ToString("yyyyMMddHHmmss"); //"2012 10 18 13 34 44";
-			string token = string.Format("500000000000000001{0}", datetime);
-			string entity = "100100300001409600100";
-			string agent = "1";
-			string c60M3OrdCode = "170008";
-			string ordCode = "410044";
+			//string datetime = DateTime.Now.ToString("yyyyMMddHHmmss"); //"2012 10 18 13 34 44";
+			//string token = string.Format("500000000000000001{0}", datetime);
+			string result = string.Empty;
+			if (UserToPassword[user] == password)
+			{
+				string token = UserToToken[user];
+				string datetime = DateTime.Now.ToString("yyyyMMddHHmmss"); //"2012 10 18 13 34 44";
+				string entity = "100100300001409600100";
+				string agent = "1";
+				string c60M3OrdCode = "170008";
+				string ordCode = "410044";
 
-			string formatXml = "<XML>" +
-				"<TOKEN>{0}</TOKEN>" +
-				"<FECHASISTEMA>{1}</FECHASISTEMA>" +
-				"<ENTITY>{2}</ENTITY>" +
-				"<MULAGENTE>{3}</MULAGENTE>" +
-				"<C60M3ORDCODE>{4}</C60M3ORDCODE>" +
-				"<ORDCODE>{5}</ORDCODE>" +
-				"</XML>";
+				string formatXml = "<XML>" +
+					"<TOKEN>{0}</TOKEN>" +
+					"<FECHASISTEMA>{1}</FECHASISTEMA>" +
+					"<ENTITY>{2}</ENTITY>" +
+					"<MULAGENTE>{3}</MULAGENTE>" +
+					"<C60M3ORDCODE>{4}</C60M3ORDCODE>" +
+					"<ORDCODE>{5}</ORDCODE>" +
+					"</XML>";
 
-			string result = string.Format(formatXml, token, datetime, entity, agent, c60M3OrdCode, ordCode);
+				result = string.Format(formatXml, token, datetime, entity, agent, c60M3OrdCode, ordCode);
+			}
 			return result;
 		}
+
+		[WebMethod]
+		public string getLegislacionCode(string token, string hash)
+		{
+			string result = string.Empty;
+			if (TokenToHash[token] == hash)
+			{
+				result = "<XML><MULIDLEGI><LEGCODLEG>AMS</LEGCODLEG><LEGDESLEG>Legislación de AMS&amp;</LEGDESLEG></MULIDLEGI><MULIDLEGI><LEGCODLEG>LSV</LEGCODLEG><LEGDESLEG>Ley &apos; de Seguridad Vial&amp;</LEGDESLEG></MULIDLEGI><MULIDLEGI><LEGCODLEG>OMC</LEGCODLEG><LEGDESLEG>Ordenanza Municipal de Circulación</LEGDESLEG></MULIDLEGI><MULIDLEGI><LEGCODLEG>RGC</LEGCODLEG><LEGDESLEG>Reglamento General de Circulación</LEGDESLEG></MULIDLEGI></XML>";
+			}
+			else
+			{
+				result = "<XML><ERROR><DESCRIPTION>ERROR: Error de Seguridad. No se ha podido encontar el algoritmo de seguridad.Error de Seguridad. No se ha podido validar el usuario</DESCRIPTION><TYPE>E</TYPE></ERROR></XML>";
+			}
+			return result;
+		}
+
+		public static Dictionary<string, string> UserToPassword
+		{
+			[System.Diagnostics.DebuggerStepThrough()]
+			get
+			{
+				if (_UserToPassword == null)
+				{
+					_UserToPassword = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+					_UserToPassword["TAO"] = "";
+				}
+				return _UserToPassword;
+			}
+		}
+		private static Dictionary<string, string> _UserToPassword;
+		public static Dictionary<string, string> UserToToken
+		{
+			[System.Diagnostics.DebuggerStepThrough()]
+			get
+			{
+				if (_UserToToken == null)
+				{
+					_UserToToken = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+					_UserToToken["TAO"] = "50000000000000000120121024054524";
+				}
+				return _UserToToken;
+			}
+		}
+		private static Dictionary<string, string> _UserToToken;
+		public static Dictionary<string, string> TokenToHash
+		{
+			[System.Diagnostics.DebuggerStepThrough()]
+			get
+			{
+				if (_TokenToHash == null)
+				{
+					_TokenToHash = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+					_TokenToHash["50000000000000000120121024054524"] = "f452289656f9c3117269a0480a0c190d";
+				}
+				return _TokenToHash;
+			}
+		}
+		private static Dictionary<string, string> _TokenToHash;
 	}
 }
